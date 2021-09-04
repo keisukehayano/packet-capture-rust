ced4e57660bf367b3e40e41990efbb5b7cf1b4f4
ohs80340 <ohs80340@ohs80340noMacBook-Air.local>
2021-09-04 18:29:15 +0900

    commitgit status

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+use pnet::datalink;
+use pnet::datalink::Channel::Ethernet;
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
+#[macro_use]
+extern crate log;
+
+use std::env;
+
+mod handler;
+mod packets;
+
+use handler::{ipv4_handler, ipv6_handler};
+
 fn main() {
-    println!("Hello, world!");
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        error!("Please specity target interface name");
+        std::process::exit(1);
+    }
+    let interface_name = &args[1];
+
+    // インターフェースの選択
+    let interfaces = datalink::interfaces();
+    let interface = interfaces
+        .into_iter()
+        .find(|iface| iface.name == *interface_name)
+        .expect("");
+
+    // データリンクのチャンネルを取得
+    let (_tx, mut rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unhandled channle type"),
+        Err(e) => {
+            panic!("Failed to create datalink channle {}", e)
+            }
+        };
+
+        match rx.next() {
+            Ok(frame) => {
+                // 受信データからイーサネットフレームの構築
+                let frame = EthernetPacket::new(frame).unwrap();
+                match frame.get_ethertype() {
+                    EtherTypes::Ipv4 => {
+                        ipv4_handler(&frame);
+                    }
+                    EtherTypes::Ipv6 => {
+                        ipv6_handler(&frame);
+                    }
+                    _ => {
+                        info!("Not a Ipv4 or Ipv6 packet");
+                    }
+                }
+            }
+            Err(e) => {
+                error!("Failed to read: {}", e);
+            }
+        }
 }
+
+
+
+
